b1185e8370b821f0003ff1bef270d5fc4942138f
Bahraz <jarekg0626@gmail.com>
2024-02-06 19:59:11 +0100

    add create village script

--- web_page/app/Controllers/createVillage.php
+++ web_page/app/Controllers/createVillage.php
@@ -4,66 +4,71 @@ include("./connection.php");
 if (session_status() == PHP_SESSION_NONE) {
     // Start the session
     session_start();
+}
 
 
-function CreateVillage($coord_x, $coord_y)
+function CreateVillage($x, $y, $id, $conn)
 {
+    // Tworzenie zapytania generującego wioskę.
+    $create_village = $conn->prepare("INSERT INTO village (`id_player`, `village_x`, `village_y`, `village_name`, `village_points`, `castle_lv`, `claypit_lv`, `ironmine_lv`, `sawmill_lv`, `warehouse_lv`, `farms_lv`, `smithy_lv`, `barracks_lv`, `walls_lv`, `commandstaff_lv`, `knightchamber_lv`) VALUES ('$id', $x,$y,'Village',46,2,1,1,1,2,2,0,0,0,0,0)");
 
+
+    $create_village->execute();
 }
 
-//TODO: połączenie do bazy danych i przeszukanie coord_x czy występuje, jeśli nie to go idziemy dalej, jeśli tak to dalej.
-try {
-    // Tworzenie nowego połączenia PDO
-    $pdo_connection = new PDO("mysql:host=$servername;dbname=$database", $db_username, $db_password);
+function CheckVillage($x,$y,$id,$conn){
+            // Tworzenie zapytania sprawdzającego istinienie wioski w danym miejscu oraz czy gracz ten posiada już wioskę.
+            $check_coordinates = $conn->prepare("SELECT * FROM village WHERE id_player = :pdo_idplayer OR village_x = $x AND village_y = $y");
 
-    // Ustawienie opcji PDO, aby wyrzucało wyjątki w przypadku błędów
-    $pdo_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $coord_x = rand(0, 50);
-    $coord_y = rand(0, 50);
-
-    echo $coord_x;
-    echo ",";
-    echo $coord_y;
-
-    // Tworzenie zapytania sprawdzającego istinienie wioski
-    $check_coordinates = $pdo_connection->prepare("SELECT * FROM village WHERE id_player = :pdo_idplayer OR village_x = $coord_x AND village_y = $coord_y");
-
-    // Przypisanie parametrów i wykonanie polecenia
-    $check_coordinates->bindParam(':pdo_idplayer', $_SESSION['player'], PDO::PARAM_STR);
-    $check_coordinates->execute();
-
-    // Pobranie wyników
-    $num_rows = $check_coordinates->rowCount();
-    // Sprawdzenie, czy użytkownik istnieje
-    if ($num_rows > 0) {
-
-        //Powtórz losowanie.
-
-    } else {
-
-        //TODO: sprawdź czy gracz posiada już wioskę.
-        // Tworzenie zapytania generującego wioskę.
-        $create_village = $pdo_connection->prepare("INSERT INTO village (`id_player`, `id_village`, `village_x`, `village_y`, `village_name`, `village_points`, `castle_lv`, `claypit_lv`, `ironmine_lv`, `sawmill_lv`, `warehouse_lv`, `farms_lv`, `smithy_lv`, `barracks_lv`, `walls_lv`, `commandstaff_lv`, `knightchamber_lv`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]','[value-9]','[value-10]','[value-11]','[value-12]','[value-13]','[value-14]','[value-15]','[value-16]','[value-17]')");
-
-
-        $create_village->execute();
-
-
-        $pdo_connection = null;
+            // Przypisanie parametrów i wykonanie polecenia
+            $check_coordinates->bindParam(':pdo_idplayer', $id, PDO::PARAM_STR);
+            $check_coordinates->execute();
+            $num_rows = $check_coordinates->rowCount();
+            return $num_rows;
 }
 
-        //TODO: Create first village in game and go to game page
+function RandCoord(){
+    $coord_x = rand(0, 10);
+    $coord_y = rand(0, 10);
+    return array($coord_x,$coord_y);
+}
 
-        $_SESSION['id_player'] = $user_data['id_player'];
-        $_SESSION['login'] = $user_data['login'];
+
+    try {
+
+        $pdo_connection = new PDO("mysql:host=$servername;dbname=$database", $db_username, $db_password);
+
+        $pdo_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $coord_x= 0;
+        $coord_y= 0;
+
+        $num_rows=CheckVillage($coord_x,$coord_y,$_SESSION['id_player'],$pdo_connection);
+
+
+    if($num_rows>0){
+
+        
+        header('Location: ../View/pages/game.php');
         session_write_close();
-
-       // header('Location: ../../index.php');
-
         exit();
-    }
+    }else{
+        do{
+            $coordinates=RandCoord();
+            $coord_x=$coordinates[0];
+            $coord_y=$coordinates[1];  
 
+            $num_rows=CheckVillage($coord_x,$coord_y,$_SESSION['id_player'],$pdo_connection);
+
+        }while($num_rows!=0);
+
+
+    }
+//TODO: połączenie do bazy danych i przeszukanie coord_x czy występuje, jeśli nie to go idziemy dalej, jeśli tak to dalej.
+    CreateVillage($coord_x, $coord_y, $_SESSION['id_player'], $pdo_connection);
+    header('Location: ../View/pages/game.php');
+
+    exit();
 } catch (PDOException $e) {
     //TODO: Dodanie skryptu, który dodaje informacje do logów serwera.
     echo "Błąd połączenia z bazą danych: " . $e->getMessage();
