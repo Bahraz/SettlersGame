8d764017a0d6109d98999d39913ea6a8cbb54098
Bahraz <jarekg0626@gmail.com>
2024-02-14 21:01:25 +0100

    change space beetwen

--- web_page/app/routers/router.php
+++ web_page/app/routers/router.php
@@ -3,16 +3,12 @@ include("../controllers/PlayerController.php");
 $playerController = new PlayerController($databaseConnection);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
     if ($_POST['action'] === 'loginPlayer') {
-
-
         $playerController->loginPlayer($_POST['login'], $_POST['password'], $_POST['action']);
 
-
     } else if ($_POST['action'] === 'logoutPlayer') {
-
-
         $playerController->logoutPlayer();
-
+        
     }
 }
