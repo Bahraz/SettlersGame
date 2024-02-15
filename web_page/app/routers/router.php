12739a5e096bf51be79d1675f72ecae36e2b2d97
Bahraz <jarekg0626@gmail.com>
2024-02-15 20:44:09 +0100

    task commit

--- web_page/app/routers/router.php
+++ web_page/app/routers/router.php
@@ -2,6 +2,10 @@
 include("../controllers/PlayerController.php");
 $playerController = new PlayerController($databaseConnection);
 
+//TODO: add routers for register
+
+//check routers method
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($_POST['action'] === 'loginPlayer') {
