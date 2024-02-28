6f7eb34dabcd33a9a437e0311ae39c0e5c860254
Bahraz <jarekg0626@gmail.com>
2024-02-28 19:25:03 +0100

    rebuild project part 2

--- webPage/src/Views/home/index.php
+++ webPage/src/Views/home/index.php
@@ -1,4 +1,11 @@
 <?php
 
-// require_once __DIR__ . '/../../../vendor/autoload.php'; 
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
+// use Bahraz\SettlersGame\views\components\HeaderViews;
+
+// $test = new HeaderViews;
+
+// $test->displayHeader();
+
+print_r(get_declared_classes());
